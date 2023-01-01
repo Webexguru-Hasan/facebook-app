800904fe145bbb7c5348f363788dc597c3deb4f9
hasan <webexguruhasan95@gmail.com>
2023-01-01 14:44:05 +0600

    created headar, navmenu, and nav button

--- src/Components/Shared/Headar/Headar.jsx
+++ src/Components/Shared/Headar/Headar.jsx
@@ -11,14 +11,16 @@ export default function Headar() {
             <div className={headar.menu}>
                 <ul>
                    <li><a href='#'>Home</a></li>
-                   <li><a href='#'>Home</a></li>
-                   <li><a href='#'>Home</a></li>
-                   <li><a href='#'>Home</a></li>
+                   <li><a href='#'>About</a></li>
+                   <li><a href='#'>Blog</a></li>
+                   <li><a href='#'>Contact</a></li>
                    
                 </ul>
             </div>
             <div className={headar.btn}>
-                <img src="logo-education.png" alt="" />
+                <button type='button'>Login</button>
+                <button type='button'>Logout</button>
+                <button type='button'>Theme Switcher</button>
             </div>
         </div>
         </>
