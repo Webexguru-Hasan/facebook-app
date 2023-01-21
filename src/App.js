9c210099469e3e1e399f201b6200189b56bcdedb
hasan <webexguruhasan95@gmail.com>
2023-01-21 18:45:54 +0600

    clear quiz-project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Blog from './Pages/Blog';
-import Contact from './Pages/Contact';
-import Home from './Pages/Home';
-import Layout from './Pages/Layout';
+
 
 
 
@@ -13,16 +9,9 @@ import Layout from './Pages/Layout';
 function App() {
   
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Layout />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/blogs' element={<Blog />} />
-        <Route path='/contact' element={<Contact />} />
+    <div>
 
-      
-    </Routes>
-    </BrowserRouter>
+    </div>
   )
 }
 
