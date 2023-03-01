5dc831ed96884d96e2899626c3e2ece6ba160401
hasan <webexguruhasan95@gmail.com>
2023-03-01 20:13:27 +0600

    [DONE]

--- src/Components/SingleProduct.jsx
+++ src/Components/SingleProduct.jsx
@@ -3,9 +3,7 @@ import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const SingleProduct = () => {
-    const {id} = useParams()
-    const [prodcut, setProduct] = useState([])
-    const [loading, setLoading] = useState(false)
+  
     return (
         <div>
             
